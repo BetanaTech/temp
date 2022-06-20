cd02f7b97c22982cbd56eac3d13d57ca81f0e577
BetanaTech <87268820+BetanaTech@users.noreply.github.com>
2022-06-20 10:49:35 +0900

    Update Test.tsx

--- Test.tsx
+++ Test.tsx
@@ -5,96 +5,37 @@ type TInput = {
   value: string;
   error: boolean;
   message: string;
-  // fulfilledにしてみた（参考：https://www.eigochigai.com/2021/08/fill-fulfill.html?m=1）
   fulfilled: boolean;
 };
 
 const initialInput: TInput = {
   value: '',
   error: false,
-  // messageに空白を入れることで加藤さんのおっしゃていた、エラーメッセージが出ることによるエレメントのズレを防止
   message: ' ',
   fulfilled: false,
 };
 
-export default function Test() {
+const errorMessage = {
+  email: 'メールアドレスを入力してください',
+};
+
+export default function TestInput_v2() {
   const [name, setName] = useState<TInput>(initialInput);
   const [email, setEmail] = useState<TInput>(initialInput);
+  const [password, setPassword] = useState<TInput>(initialInput);
+  const [cfmPassword, setCfmPassword] = useState<TInput>(initialInput);
 
-  // useRefの使い方：https://qiita.com/seira/items/0e6a2d835f1afb50544d
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
 
-  // 下記、useRefでバリデーションチェックしていますが、これまでの方法でも大丈夫です。
-  // 下記のやり方で名前とメールのチェックを同じ関数にまとめられた
-  const checkInput = (
-    value: string,
-    data: TInput,
-    setData: Dispatch<SetStateAction<TInput>>,
-    ref: HTMLInputElement
-  ) => {
-    // inputPropsで設定した条件に合わない場合（不正な場合）
-    if (!ref.validity.valid) {
-      // データがnullの場合
-      if (!value) {
-        setData({ ...data, value, error: true, message: '必須項目です' });
-        // データが不正な場合
-      } else {
-        setData({ ...data, value, error: true, message: '不正な値です' });
-      }
-      // inputPropsで設定した条件に合う場合（正な場合）
-    } else {
-      // messageに空白を入れることで加藤さんのおっしゃていた、エラーメッセージが出ることによるエレメントのズレを防止
-      setData({ ...data, value, error: false, message: ' ', fulfilled: true });
-    }
-  };
-
-  const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    data: TInput,
-    setData: Dispatch<SetStateAction<TInput>>,
-    dataRef: RefObject<HTMLInputElement>
-  ) => {
-    const value = event.target.value;
-    const ref = dataRef.current;
-    // refがnullかどうかチェック（必須）
-    if (ref) {
-      // 正な場合　もしくは　入力済み　の場合（＝一旦、正な入力ができた場合）
-      if (ref.validity.valid || data.fulfilled) {
-        // console.log(ref.validity.valid);
-        // console.log(ref.validationMessage);
-
-        checkInput(value, data, setData, ref);
-
-        // まだ正な入力ができていない場合
-      } else {
-        setData({ ...data, value });
-      }
-      // refがnullの場合
-    } else {
-      // 原則起きないが、起きた場合を考慮して、今後エラーハンドリングが必要かも
-    }
-  };
-
-  const handleBlur = (
-    event: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>,
-    data: TInput,
-    setData: Dispatch<SetStateAction<TInput>>,
-    dataRef: RefObject<HTMLInputElement>
-  ) => {
-    const value = event.target.value;
-    const ref = dataRef.current;
-    // refがnullかどうかチェック（必須）
-    if (ref) {
-      checkInput(value, data, setData, ref);
-      // refがnullの場合
-    } else {
-      // 原則起きないが、起きた場合を考慮して、今後エラーハンドリングが必要かも
-    }
-  };
-
   const isInvalid = (): boolean => {
-    return !(!name.error && name.fulfilled && !email.error && email.fulfilled);
+    return !(
+      //   !name.error &&
+      //   name.fulfilled &&
+      //   !email.error &&
+      //   email.fulfilled &&
+      (!password.error && password.fulfilled && !cfmPassword.error && cfmPassword.fulfilled)
+    );
   };
 
   return (
@@ -102,43 +43,33 @@ export default function Test() {
       <h1>Test</h1>
       <div>
         <p className="m-2">入力</p>
-        <TextField
-          className="m-2"
-          style={{ width: '80%' }}
-          // maxLengthとpatternはテスト用です
-          inputProps={{ required: true, maxLength: 10, pattern: '^[a-zA-Z0-9_]+$' }}
-          inputRef={nameRef}
-          error={name.error}
-          label="名前"
-          name="name"
-          // エラーではない　かつ　入力済み　の場合、緑色＋フォーカス
-          color={!name.error && name.fulfilled ? 'success' : undefined}
-          focused={!name.error && name.fulfilled}
-          value={name.value}
-          onChange={(event) => handleChange(event, name, setName, nameRef)}
-          onBlur={(event) => handleBlur(event, name, setName, nameRef)}
-          helperText={name.message}
-          // チェックマークはなくてもいいかなと思いました（POに要相談）
+        <CTextField
+          dataLabel={'名前'}
+          dataName={'name'}
+          dataType={'text'}
+          data={name}
+          setData={setName}
+          dataProps={{ required: true }}
+          dataRef={nameRef}
         />
-        <TextField
-          className="m-2"
-          style={{ width: '80%' }}
-          // メールのチェックは「type="email"」でやってみた
-          type="email"
-          inputProps={{ required: true }}
-          inputRef={emailRef}
-          error={email.error}
-          label="メール"
-          name="email"
-          // エラーではない　かつ　入力済み　の場合、緑色＋フォーカス
-          color={!email.error && email.fulfilled ? 'success' : undefined}
-          focused={!email.error && email.fulfilled}
-          value={email.value}
-          onChange={(event) => handleChange(event, email, setEmail, emailRef)}
-          onBlur={(event) => handleBlur(event, email, setEmail, emailRef)}
-          helperText={email.message}
+        <CTextField
+          dataLabel={'メールアドレス'}
+          dataName={'email'}
+          dataType={'email'}
+          data={email}
+          setData={setEmail}
+          dataProps={{ required: true }}
+          dataRef={emailRef}
+          errorMessage={errorMessage.email}
+        />
+        <CPasswordField
+          password={password}
+          cfmPassword={cfmPassword}
+          setPassword={setPassword}
+          setCfmPassword={setCfmPassword}
         />
       </div>
+
       <div>
         <button
           type="button"
@@ -149,6 +80,215 @@ export default function Test() {
           クリック
         </button>
       </div>
+    </>
+  );
+}
+
+// -------------------- TextField --------------------
+function CTextField({
+  dataLabel,
+  dataName,
+  dataType,
+  data,
+  setData,
+  dataProps,
+  dataRef,
+  errorMessage = '不正な値です',
+}: {
+  dataLabel: string;
+  dataName: string;
+  dataType: string;
+  data: TInput;
+  setData: Dispatch<SetStateAction<TInput>>;
+  dataProps: {};
+  dataRef: RefObject<HTMLInputElement>;
+  errorMessage?: string;
+}) {
+  const checkInput = (value: string, ref: HTMLInputElement) => {
+    if (!ref.validity.valid) {
+      if (!value) {
+        setData({ ...data, value, error: true, message: '必須項目です' });
+      } else {
+        setData({ ...data, value, error: true, message: errorMessage });
+      }
+    } else {
+      setData({ ...data, value, error: false, message: ' ', fulfilled: true });
+    }
+  };
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    dataRef: RefObject<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+    const ref = dataRef.current;
+    if (ref) {
+      if (ref.validity.valid || data.fulfilled) {
+        checkInput(value, ref);
+      } else {
+        setData({ ...data, value });
+      }
+    } else {
+    }
+  };
+
+  const handleBlur = (
+    event: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>,
+    dataRef: RefObject<HTMLInputElement>
+  ) => {
+    const value = event.target.value;
+    const ref = dataRef.current;
+    if (ref) {
+      checkInput(value, ref);
+    } else {
+    }
+  };
+
+  return (
+    <TextField
+      className="m-2"
+      style={{ width: '80%' }}
+      label={dataLabel}
+      name={dataName}
+      type={dataType}
+      inputProps={dataProps}
+      inputRef={dataRef}
+      error={data.error}
+      color={!data.error && data.fulfilled ? 'success' : undefined}
+      focused={!data.error && data.fulfilled}
+      value={data.value}
+      onChange={(event) => handleChange(event, dataRef)}
+      onBlur={(event) => handleBlur(event, dataRef)}
+      helperText={data.message}
+    />
+  );
+}
+
+// -------------------- Password --------------------
+function CPasswordField({
+  password,
+  cfmPassword,
+  setPassword,
+  setCfmPassword,
+}: {
+  password: TInput;
+  cfmPassword: TInput;
+  setPassword: Dispatch<SetStateAction<TInput>>;
+  setCfmPassword: Dispatch<SetStateAction<TInput>>;
+}) {
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const cfmPasswordRef = useRef<HTMLInputElement>(null);
+
+  const errorPasswordMessage: string =
+    '半角英字と半角数字をそれぞれ1文字以上含む半角英数字6文字以上12文字以下で入力してください';
+  const errorCfmPasswordMessage: string = 'パスワードが一致しません';
+
+  const passwordPattern = '^[a-zA-Z0-9]{6,24}';
+
+  const checkPassword = (
+    value: string,
+    ref: HTMLInputElement,
+    password: TInput,
+    setPassword: Dispatch<SetStateAction<TInput>>,
+    errorMessage: string
+  ) => {
+    if (!ref.validity.valid) {
+      if (!value) {
+        setPassword({ ...password, value, error: true, message: '必須項目です' });
+      } else {
+        setPassword({
+          ...password,
+          value,
+          error: true,
+          message: errorMessage,
+        });
+      }
+    } else {
+      setPassword({ ...password, value, error: false, message: ' ', fulfilled: true });
+    }
+  };
+
+  const handleChangePassword = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    passwordRef: RefObject<HTMLInputElement>,
+    password: TInput,
+    setPassword: Dispatch<SetStateAction<TInput>>,
+    errorMessage: string
+  ) => {
+    const value = event.target.value;
+    const ref = passwordRef.current;
+    if (ref && cfmPasswordRef.current) {
+      if (ref.validity.valid || password.fulfilled) {
+        checkPassword(value, ref, password, setPassword, errorMessage);
+      } else {
+        setPassword({ ...password, value });
+      }
+      if ((cfmPassword.fulfilled || cfmPassword.message !== ' ') && ref.name === 'password') {
+        const customRef: HTMLInputElement = {
+          validity: {
+            valid: value === cfmPassword.value,
+          },
+        } as HTMLInputElement;
+        checkPassword(cfmPassword.value, customRef, cfmPassword, setCfmPassword, errorCfmPasswordMessage);
+      } else {
+      }
+    } else {
+    }
+  };
+
+  const handleBlurPassword = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    passwordRef: RefObject<HTMLInputElement>,
+    password: TInput,
+    setPassword: Dispatch<SetStateAction<TInput>>,
+    errorMessage: string
+  ) => {
+    const value = event.target.value;
+    const ref = passwordRef.current;
+    if (ref) {
+      checkPassword(value, ref, password, setPassword, errorMessage);
+    } else {
+    }
+  };
+
+  return (
+    <>
+      <TextField
+        className="m-2"
+        style={{ width: '80%' }}
+        type="password"
+        inputProps={{ required: true, pattern: passwordPattern }}
+        inputRef={passwordRef}
+        error={password.error}
+        label="パスワード"
+        name="password"
+        color={!password.error && password.fulfilled ? 'success' : undefined}
+        focused={!password.error && password.fulfilled}
+        value={password.value}
+        onChange={(event) => handleChangePassword(event, passwordRef, password, setPassword, errorPasswordMessage)}
+        onBlur={(event) => handleBlurPassword(event, passwordRef, password, setPassword, errorPasswordMessage)}
+        helperText={password.message}
+      />
+      <TextField
+        className="m-2"
+        style={{ width: '80%' }}
+        type="password"
+        inputProps={{ required: true, pattern: `^${password.value}$` }}
+        inputRef={cfmPasswordRef}
+        error={cfmPassword.error}
+        label="確認用パスワード"
+        name="cfmPassword"
+        color={!cfmPassword.error && cfmPassword.fulfilled ? 'success' : undefined}
+        focused={!cfmPassword.error && cfmPassword.fulfilled}
+        value={cfmPassword.value}
+        onChange={(event) => {
+          handleChangePassword(event, cfmPasswordRef, cfmPassword, setCfmPassword, errorCfmPasswordMessage);
+        }}
+        onBlur={(event) =>
+          handleBlurPassword(event, cfmPasswordRef, cfmPassword, setCfmPassword, errorCfmPasswordMessage)
+        }
+        helperText={cfmPassword.message}
+      />
     </>
   );
 }
